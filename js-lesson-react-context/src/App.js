122789d9ad22b5848f50b247e9f96b57d4f31edd
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-06 16:36:37 +0100

    Add Browser Router

--- js-lesson-react-context/src/App.js
+++ js-lesson-react-context/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 
+
 function App() {
   return (
-    <div>
-      <h1>Holiday Gift Generation</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Holiday Gift Generation</h1>
+        </div>
+      </div>
     </div>
   );
 }
