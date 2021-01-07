97e64b94f404d52fdddcec12ae4abd3f3808bced
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-07 17:20:29 +0100

    Add useState and useEffect for ResulPage

--- js-lesson-react-context/src/pages/ResultPage.jsx
+++ js-lesson-react-context/src/pages/ResultPage.jsx
@@ -1,16 +1,22 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import { UserInputContext } from '../contexts/UserInputContex'
 import {GiftAI} from '../GiftAI'
 
 export default function ResultPage() {
   const {age, interest, relationship} = useContext(UserInputContext);
-  const code = `${age}${interest}${relationship}`
+  const code = `${age}${interest}${relationship}`;
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout( () => setIsLoading(false), 3000 )
+  }, [])
 
   return (
     <div>
       <h2>Result</h2>
+
       <p>
-        {GiftAI.calculate(code)}
+        { isLoading ? "Loading" : GiftAI.calculate(code)}
       </p>
       
     </div>
