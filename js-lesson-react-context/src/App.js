dfdd0cb46e28c63469b27e7e4f9e69fe5d92f1f5
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-07 15:23:28 +0100

    Add Context map and UserInputContext.js. Update App with UserInputContext.Provider

--- js-lesson-react-context/src/App.js
+++ js-lesson-react-context/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import AgePage from './pages/AgePage'
 import InterestPage from './pages/InterestPage';
 import RelationshipPage from './pages/RelationshipPage';
 import ResultPage from './pages/ResultPage';
+import { UserInputContext } from './contexts/UserInputContex';
 
 function App() {
+  const [age, setAge] = useState("");
+  const [interest, setInterst] = useState("");
+  const [relationship, setRelationship] = useState("");
+
+  const userInputContextValue = {
+    age,setAge,
+    interest, setInterst,
+    relationship, setRelationship
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -16,23 +27,26 @@ function App() {
       </div>
 
       <div className="col-md-12">
-        <Switch>
-          <Route path="/result">
-            <ResultPage />
-          </Route>
-          <Route path="/relationship">
-            <RelationshipPage />
-          </Route>
-          <Route path="/interest">
-            <InterestPage />
-          </Route>
-          <Route path="/age">
-            <AgePage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
+        <UserInputContext.Provider value={userInputContextValue}>
+          <Switch>
+            <Route path="/result">
+              <ResultPage />
+            </Route>
+            <Route path="/relationship">
+              <RelationshipPage />
+            </Route>
+            <Route path="/interest">
+              <InterestPage />
+            </Route>
+            <Route path="/age">
+              <AgePage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </UserInputContext.Provider>
+        
       </div>
     </div>
   );
