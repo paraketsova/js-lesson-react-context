f32cbc4620f350680ee080f8abc390b639fa861d
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-07 17:37:16 +0100

    Update ResultPage - add function (img and text) for loading and loaded stage

--- js-lesson-react-context/src/pages/ResultPage.jsx
+++ js-lesson-react-context/src/pages/ResultPage.jsx
@@ -7,16 +7,37 @@ export default function ResultPage() {
   const code = `${age}${interest}${relationship}`;
   const [isLoading, setIsLoading] = useState(true)
 
+  const loadingImg = "https://media.giphy.com/media/K90ckojkohXfW/giphy.gif";
+  const loadedImg = "https://media.giphy.com/media/l0HlTJY5owTDq1jKo/giphy.gif";
+
   useEffect(() => {
-    setTimeout( () => setIsLoading(false), 3000 )
+    setTimeout( () => setIsLoading(false), 3000)
   }, [])
+
+  function renderIsLoading() {
+    return(
+      <>
+        <img className="img-fluid" src={loadingImg} />
+        <p>Contacting SANTA DOT AI</p>
+      </>
+    )
+  }
+
+  function renderIsLoaded() {
+    return(
+      <>
+        <img className="img-fluid" src={loadedImg} />
+        <h3>You should buy: {GiftAI.calculate(code)}</h3>
+      </>
+    )
+  }
 
   return (
     <div>
       <h2>Result</h2>
 
       <p>
-        { isLoading ? "Loading" : GiftAI.calculate(code)}
+        { isLoading ? renderIsLoading() : renderIsLoaded()}
       </p>
       
     </div>
