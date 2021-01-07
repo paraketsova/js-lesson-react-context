1c08da965078bc6731caf031d23af89252d7599e
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-07 15:32:41 +0100

    Add useContext to ResultPage

--- js-lesson-react-context/src/pages/ResultPage.jsx
+++ js-lesson-react-context/src/pages/ResultPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { UserInputContext } from '../contexts/UserInputContex'
 
 export default function ResultPage() {
+  const {age, interest, relationship} = useContext(UserInputContext);
+
   return (
     <div>
       <h2>Result</h2>
+      <p>{age}</p>
+      <p>{interest}</p>
+      <p>{relationship}</p>
       
     </div>
   )
